67f88d14c75bbe31572af18202d3d2adc583ba59
Zujuy <itza_zujuy@hotmail.co.uk>
2025-02-07 15:41:23 -0600

    adding home

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Bienvenido a Hybrid Soldiers México!</h1>
+      <p className="home-description">
+        Join our community of passionate Linkin Park fans. Stay updated on the latest events, connect with other fans, and become an official member today!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
