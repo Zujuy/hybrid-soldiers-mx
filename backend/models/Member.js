c1b92ece410f5d70fcf02cea080090b453c6c52d
Zujuy <itza_zujuy@hotmail.co.uk>
2025-02-07 15:10:54 -0600

    create Member model

---
+++ backend/models/Member.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const MemberSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  memberNumber: { type: String, required: true, unique: true },
+});
+
+module.exports = mongoose.model('Member', MemberSchema);
