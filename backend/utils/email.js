75c1c0930a38efdd3e6695a7bb99cb8756511a19
Zujuy <itza_zujuy@hotmail.co.uk>
2025-02-07 15:12:32 -0600

    creating email utils

---
+++ backend/utils/email.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendCredentialEmail = async (email, name, memberNumber, photoPath) => {
+  const credentialHtml = `
+    <div style="border: 2px solid black; padding: 20px; text-align: center;">
+      <img src="cid:logo" style="width: 50px; position: absolute; top: 10px; right: 10px;">
+      <img src="cid:photo" style="width: 100px; border-radius: 50%; margin-bottom: 20px;">
+      <h1>${name}</h1>
+      <p style="font-size: 12px; font-weight: light;">Member #${memberNumber}</p>
+    </div>
+  `;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: 'Your Linkin Park Fan Club Credential',
+    html: credentialHtml,
+    attachments: [
+      {
+        filename: 'logo.png',
+        path: path.join(__dirname, '../assets/logo.png'),
+        cid: 'logo',
+      },
+      {
+        filename: 'photo.png',
+        path: photoPath,
+        cid: 'photo',
+      },
+    ],
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendCredentialEmail };
